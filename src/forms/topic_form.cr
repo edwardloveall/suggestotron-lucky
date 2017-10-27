8acd9fdf1eceaa443d428ccc313e1de1b15da514
Edward Loveall <edward@edwardloveall.com>
2017-10-27 16:59:12 -0400

    Add length validation to title

--- src/forms/topic_form.cr
+++ src/forms/topic_form.cr
@@ -1,4 +1,14 @@
 class TopicForm < Topic::BaseForm
   allow title
   allow description
+
+  def call
+    validate_length title, min: 10
+  end
+
+  private def validate_length(field, min)
+    if (field.value || "").size < min
+      field.add_error "is too short"
+    end
+  end
 end
